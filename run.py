33cd68a934a47c9318cca8907ef6e03630be564b
Megalomizer <112645496+Megalomizer@users.noreply.github.com>
2024-04-09 12:21:03 +0200

    Made 2 btns to change text

--- run.py
+++ run.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static', )
 
+global snifferActivityTracker;
+global spooferActivityTracker;
+snifferActivityTracker = "Inactive";
+spooferActivityTracker = "Inactive";
+
 @app.route("/")
-def home():
-    return render_template('activatescripts.html')
+def home(snifferbtntext=snifferActivityTracker, spooferbtntext=spooferActivityTracker):
+    return render_template('activatescripts.html', snifferbtnactivity=snifferbtntext, spooferbtnactivity=spooferbtntext)
 
 @app.route("/activatescripts")
 def goScripts():
@@ -21,6 +26,26 @@ def goCalories():
 @app.route("/sleeprythm")
 def goSleep():
     return render_template('sleeprythm.html')
+
+@app.route("/activatescripts/changeSnifferActivity")
+def goScriptsBtn1():
+    global snifferActivityTracker;
+    global spooferActivityTracker;
+    if snifferActivityTracker=="Active":
+        snifferActivityTracker="Inactive"
+    else:
+        snifferActivityTracker="Active"
+    return home(spooferbtntext=spooferActivityTracker, snifferbtntext=snifferActivityTracker)
+
+@app.route("/activatescripts/changeSpooferActivity")
+def goScriptsBtn2():
+    global snifferActivityTracker;
+    global spooferActivityTracker;
+    if spooferActivityTracker=="Active":
+        spooferActivityTracker="Inactive"
+    else:
+        spooferActivityTracker="Active"
+    return home(spooferbtntext=spooferActivityTracker, snifferbtntext=snifferActivityTracker)
 
 if __name__ == "__main__":
     app.run(debug=True)
